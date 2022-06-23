f9d7c08a2e672271542133ed7a8dde41708b701a
Alexey Kartashev <68597810+alexOnOff@users.noreply.github.com>
2022-06-23 12:10:56 +0300

    Add State files

---
+++ ExamplesPatterns/State/SubscriptionState.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+  internal class SubscriptionState : State
+  {
+    public override void BuySubscription(Service service)
+    {
+      Console.WriteLine("Подписка уже куплена\n");
+    }
+
+    public override void DownlandMusic()
+    {
+      Console.WriteLine("Музыка скачивается...");
+      Console.WriteLine("Завершено!\n");
+    }
+
+    public override void GetTrialPeriod(Service service)
+    {}
+
+    public override void OrderFood()
+    {
+      Console.WriteLine("Курьер доставит заказ в течение 10-ти минут.\n");
+    }
+
+    public override void Unsubscribe(Service service)
+    {
+      Console.WriteLine("Подписка отменена!\n");
+      service.state = new WithoutSubscriptionState();
+      service.isNewUser = false;
+    }
+
+    public override void WatchFilm()
+    {
+      Console.WriteLine("Наслаждайтесь просмотром!\n");
+    }
+  }
+}
---
+++ ExamplesPatterns/State/WithoutSubscriptionState.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+  internal class WithoutSubscriptionState : State
+  {
+    public override void BuySubscription(Service service)
+    {
+      service.state = new SubscriptionState();
+    }
+
+    public override void DownlandMusic()
+    {
+      Console.WriteLine("Вы не можете скачать музыку на свое устройство!\n");
+    }
+
+    public override void GetTrialPeriod(Service service)
+    {
+      if(service.isNewUser == true){
+        service.state = new TrialState();
+      }
+      else{
+        Console.WriteLine("Вы уже использовали пробный период\n");
+      }
+    }
+
+    public override void OrderFood()
+    {
+      Console.WriteLine("Курьер доставит заказ в течение часа.\n");
+    }
+
+    public override void Unsubscribe(Service service)
+    { }
+
+    public override void WatchFilm()
+    {
+      Console.WriteLine("*Реклама*");
+      Console.WriteLine("Наслаждайтесь просмотром!");
+      Console.WriteLine("*Реклама*\n");
+    }
+  }
+}
